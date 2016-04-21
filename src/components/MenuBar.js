e74ccfc698019192c9019ab4fc206fe3f893f039
Don Henton <donhenton@gmail.com>
2016-04-21 15:14:03 -0500

    starting on menu component 2

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -16,44 +16,22 @@ import { Link } from 'react-router';
 
         render() {
         return (
-                <div className="menuBarContainer">
-                      <ul className="menubar">
-                         <li className="menubar-item">
-                           <Link to="/" className="menu-item-target" >Home</Link>
-                         </li>
-                
-                         <li className="menubar-item">
-                           <span className="menu-item-target" onclick >List Demos</span>
-                           <ul className="menu">
-                             <li className="menu-item">
-                               <Link to="/listdemoRow" className="menu-item-target" >List Demo with Row</Link>
-                            </li>
-                            <li className="menu-item">
-                               <Link to="/listdemoNoRow" className="menu-item-target" >List Demo with No Row</Link>
-                            </li>
-                           </ul>
-                         </li>
-                
-                
-                
-                
-                
-                      </ul>
-                
-      
-      <Link to="/page2" className="mainMenuItem" >
-        Page2
-      </Link>
-      <Link to={`/page2/${this.id}`} className="mainMenuItem" >
-        Page2 with 20
-      </Link>
-      <Link to="/listdemoRow" className="mainMenuItem" >
-        List Demo with Row
-      </Link>
-      <Link to="/listdemoNoRow" className="mainMenuItem" >
-        List Demo with No Row
-      </Link>
-      
+      <div className="menuBarContainer">
+        <Link to="/page2" className="mainMenuItem" >
+          Page2
+        </Link>
+        <Link to={`/page2/${this.id}`} className="mainMenuItem" >
+          Page2 with 20
+        </Link>
+        <Link to="/listdemoRow" className="mainMenuItem" >
+          List Demo with Row
+        </Link>
+        <Link to="/listdemoNoRow" className="mainMenuItem" >
+          List Demo with No Row
+        </Link>
+        <Link to="/topNav" className="mainMenuItem" >
+          Top Nav
+        </Link>
       </div>
 
                 );
---
+++ src/components/TopNavPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Component } from 'react';
+import Container from './ContentContainer';
+
+export default class TopNavPage extends Component {
+        
+ 
+        
+  render() {
+    return (
+      
+      <Container>
+        top nav page
+      </Container>
+      
+       
+    );
+  }
+}
