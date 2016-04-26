5b11e94d2d4b89dd3302737fd7a0bb9a01a78abd
Don Henton <donhenton@gmail.com>
2016-04-26 07:49:43 -0500

    refactored to placed restaurant in its own class

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,14 +1,10 @@
 import React from 'react';
-        import { Component } from 'react';
-        import Container from './components/ContentContainer';
-        import {RESTAURANT_SERVICE} from './components/App';
-        import {EMPTY_RESTAURANT} from './components/restaurant/restaurantService';
-        import {arrayToIterable} from './components/restaurant/restaurantUtils';
-        import postal from 'postal';
-        import EditRestaurantForm from './components/restaurant/editRestaurantForm';
-        import ListItem from './components/restaurant/restaurantListItem';
+import { Component } from 'react';
+import Container from './components/ContentContainer';
+import RestaurantApp from './components/restaurant/restaurantApp';
+
         
-        export default class RestaurantApp extends Component {
+        export default class HomePage extends Component {
 
         constructor()
         {
@@ -16,93 +12,7 @@ import React from 'react';
          
 
         }
-        componentWillMount()
-        {
-                let me = this;
-                this.state = {items: [EMPTY_RESTAURANT()],actionMode: null,hideList: true};
-                RESTAURANT_SERVICE.getAllRestaurants()
-
-                .then(function (data) {
-                    
-                    var preppedData =  JSON.parse(data);
-                         
-                        me.setState({items: preppedData,actionMode: null,hideList: false}, function() {
-                             
-                            
-                        })
-                        
-                })
-                .catch(function (err) {
-                throw err;
-                });
-                
-                
-                //register postal listeners
-                
-                postal.subscribe({
-                    channel: "restaurants",
-                    topic: "item.save.request.complete",
-                    callback: function (data, envelope) {
-                        me.processSaveComplete(data,envelope)
-                    }
-                }); 
-
-                postal.subscribe({
-                    channel: "restaurants",
-                    topic: "item.edit.cancel",
-                    callback: function (data, envelope) {
-                        me.processEditCancel(data,envelope)
-                    }
-                }); 
-
-                postal.subscribe({
-                    channel: "restaurants",
-                    topic: "edit.Item",
-                    callback: function (data, envelope) {
-
-                        me.setState({'actionMode': "EDIT"},function( )
-                            {
-                                  //this is called when state is finally set
-
-
-
-                            })
-
-                       }
-                    }); 
-
-                
-                
-                
-                
-                
-                
-                
-             
-        }
-        
- hideTableClass()
- {
-     if (this.state.hideList)
-     {
-         return "hidden";
-     }
-     return null;
- }
-
- displayEditFormCSS()
-  {
-      //console.log("display css "+this.state.inEditMode);
-      if (this.state.actionMode)
-      {
-         return "editRestaurantContainer";
-      }
-      else
-      {
-          return "editRestaurantContainer hidden"; 
-      }
       
-  }
 
         render() {
             let me = this;
@@ -110,58 +20,7 @@ import React from 'react';
         return (
                 < Container >
                 < h2 > Restaurant Demo < /h2>
-            <div className='restaurantApp grouping'>
-                <div className='restaurantListContainer'>
-                    <div>
-                        <button   className="editButton addButton">Add Record</button>
-                    </div>
-            
-                    <div id="restaurantScrollList">
-                            <div id="tHeadContainer">
-                                <table>
-                                    <tbody>
-                                        <tr>
-                                            <th className="nameItem">Name</th> 
-                                            <th className="cityItem">City</th> 
-                                            <th className="stateItem">State</th> 
-                                            <th className="zipCodeItem">Zip Code</th>
-                                            <th className="versionItem">Version</th>
-                                            <th className="actionItems">&nbsp;</th> 
-                                            <th className="actionItems">&nbsp;</th> 
-                                             
-                                        </tr>
-                                    </tbody>
-                                 </table>    
-                            </div>    
-                            <div id='tbodyContainer' className={this.hideTableClass()}>
-                            <table>
-                            <tbody>
-                                {
-
-                                     
-                                    this.state.items.map((item,i) => (
-
-                                        <ListItem key={item.id} item={item} />
-                                    ))
-
-
-                                }
-                            </tbody>
-
-                            </table>
-                            </div>
-                    </div>
-                    
-            
-            
-            
-                </div>    
-             <div className={me.displayEditFormCSS()}>
-                        <EditRestaurantForm />
-                    </div>
-                  
-            
-            </div> 
+                <RestaurantApp />
                 < /Container>
 
 
---
+++ src/components/restaurant/RestaurantApp.js
@@ -0,0 +1,173 @@
+    import React from 'react';
+    import { Component } from 'react';
+
+    import {RESTAURANT_SERVICE} from './../App';
+    import {EMPTY_RESTAURANT} from './restaurantService';
+    import {arrayToIterable} from './restaurantUtils';
+    import postal from 'postal';
+    import EditRestaurantForm from './editRestaurantForm';
+    import ListItem from './restaurantListItem';
+
+    export default class RestaurantApp extends Component {
+
+    constructor()
+    {
+    super();
+
+
+    }
+    componentWillMount()
+    {
+            let me = this;
+            this.state = {items: [EMPTY_RESTAURANT()],actionMode: null,isLoading: true};
+            RESTAURANT_SERVICE.getAllRestaurants()
+
+            .then(function (data) {
+
+                var preppedData =  JSON.parse(data);
+
+                    me.setState({items: preppedData,actionMode: null,isLoading: false}, function() {
+
+
+                    })
+
+            })
+            .catch(function (err) {
+            throw err;
+            });
+
+
+            //register postal listeners
+
+            postal.subscribe({
+                channel: "restaurants",
+                topic: "item.save.request.complete",
+                callback: function (data, envelope) {
+                    me.processSaveComplete(data,envelope)
+                }
+            }); 
+
+            postal.subscribe({
+                channel: "restaurants",
+                topic: "item.edit.cancel",
+                callback: function (data, envelope) {
+                    me.processEditCancel(data,envelope)
+                }
+            }); 
+
+            postal.subscribe({
+                channel: "restaurants",
+                topic: "edit.Item",
+                callback: function (data, envelope) {
+
+                    me.setState({'actionMode': "EDIT"},function( )
+                        {
+                              //this is called when state is finally set
+
+
+
+                        })
+
+                   }
+                }); 
+
+
+
+
+
+
+
+
+
+    }
+
+    hideTableClass()
+    {
+     if (this.state.isLoading)
+     {
+         return "hidden";
+     }
+     return null;
+    }
+
+    displayEditFormCSS()
+    {
+      //console.log("display css "+this.state.inEditMode);
+      if (this.state.actionMode)
+      {
+         return "editRestaurantContainer";
+      }
+      else
+      {
+          return "editRestaurantContainer hidden"; 
+      }
+
+    }
+
+        render() {
+            let me = this;
+
+        return (
+
+
+            <div className='restaurantApp grouping'>
+                <div className='restaurantListContainer'>
+                    <div>
+                        <button   className="editButton addButton">Add Record</button>
+                    </div>
+
+                    <div id="restaurantScrollList">
+                            <div id="tHeadContainer">
+                                <table>
+                                    <tbody>
+                                        <tr>
+                                            <th className="nameItem">Name</th> 
+                                            <th className="cityItem">City</th> 
+                                            <th className="stateItem">State</th> 
+                                            <th className="zipCodeItem">Zip Code</th>
+                                            <th className="versionItem">Version</th>
+                                            <th className="actionItems">&nbsp;</th> 
+                                            <th className="actionItems">&nbsp;</th> 
+
+                                        </tr>
+                                    </tbody>
+                                 </table>    
+                            </div>    
+                            <div id='tbodyContainer' className={this.hideTableClass()}>
+                            <table>
+                            <tbody>
+                                {
+
+
+                                    this.state.items.map((item,i) => (
+
+                                        <ListItem key={item.id} item={item} />
+                                    ))
+
+
+                                }
+                            </tbody>
+
+                            </table>
+                            </div>
+                    </div>
+
+
+
+
+                </div>    
+                <div className={me.displayEditFormCSS()}>
+                        <EditRestaurantForm />
+                </div>
+
+
+            </div> 
+
+
+
+
+                );
+        }
+    }
+
+
