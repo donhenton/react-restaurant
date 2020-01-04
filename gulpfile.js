6c0be557ef7b3382b290d487a51b745d94964b65
Don Henton <donhenton@gmail.com>
2020-01-04 00:33:12 -0600

    working on aws adaptation

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@
  * 
  */
 
-
+var browserSync = require('browser-sync').create();
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
@@ -24,8 +24,8 @@ var browserify = require('browserify');
 var watch = require('gulp-watch');
 var tap = require('gulp-tap');
 var gutil = require('gulp-util');
-var server = require('gulp-server-livereload');
-var livereload = require('gulp-livereload');
+//var server = require('gulp-server-livereload');
+//var livereload = require('gulp-livereload');
 var gulpsync = require('gulp-sync')(gulp);
 var gulpif = require("gulp-if");
 var argv = require('yargs').argv;
@@ -33,12 +33,8 @@ var rename = require("gulp-rename");
 
 
 var notify = require("./build_utils/build_utils").notify;
-var targetLocation = './target/'
-/* livereload loads this page you only get one  
- * 
- * the chrome livereload plugin needs to be installed
- * 
- */
+var targetLocation = './public_html/'
+
 var pageURL = 'http://localhost:8080';
 
 var SASS_FILES = './sass/**/*.scss';
@@ -49,7 +45,9 @@ function Bundle() {
 
     var Bundler = browserify({
         entries: './src/index.js',
-        transform: [["babelify", {"presets": ["es2015","react"]}]],
+        transform: [
+            ["babelify", { "presets": ["es2015", "react"] }]
+        ],
         extensions: ['.js'],
         debug: true,
         cache: {},
@@ -57,122 +55,109 @@ function Bundle() {
         fullPaths: true
     });
     return Bundler
-            .bundle()
-            .on('error', notify);
+        .bundle()
+        .on('error', notify);
 }
 
-gulp.task('copy-html', function () {
+gulp.task('copy-html', function() {
 
 
     // base allows to copy the folders above the file
     // return gulp.src(MAIN_HTML_FILE,{'cwd': './src/html','base':'./..'} )
     return gulp.src(MAIN_HTML_FILE).pipe(gulp.dest(targetLocation))
-            .on('finish', function ( ) {
-                gutil.log("processing change in html");
-                livereload.reload(pageURL);
-               // cb();
-            });
- 
+        .on('finish', function() {
+            gutil.log("processing change in html");
+            //livereload.reload(pageURL);
+            // cb();
+        });
+
 
 });
 
-gulp.task('clean', function (  ) {
+gulp.task('clean', function() {
 
     del(['target']);
 
 });
 
-gulp.task('build', function () {
+gulp.task('build', function() {
     Bundle()
-            .pipe(source('bundle.js'))
-            .pipe(gulpif(argv.production, streamify(uglify())))
-       //     .pipe(gulpif(argv.production, rename({suffix: '.min'})))
-            .pipe(gulp.dest(targetLocation))
-            .on('finish', function ( ) {
-                gutil.log("build bundle end");
-                 livereload.reload(pageURL);
-            });
-    ;
+        .pipe(source('bundle.js'))
+        .pipe(gulpif(argv.production, streamify(uglify())))
+        //     .pipe(gulpif(argv.production, rename({suffix: '.min'})))
+        .pipe(gulp.dest(targetLocation))
+        .on('finish', function() {
+            gutil.log("build bundle end");
+            // livereload.reload(pageURL);
+        });;
 });
 var sassProcess =
-        function () {
+    function() {
 
-            return gulp.src(SASS_FILES)
-                    .pipe(sass().on('error', sass.logError))
-                    .pipe(concat('restaurantStyles.css'))
-                  //  .pipe(uglifycss())
-                    .pipe(gulp.dest(targetLocation));
-        };
+        return gulp.src(SASS_FILES)
+            .pipe(sass().on('error', sass.logError))
+            .pipe(concat('restaurantStyles.css'))
+            //  .pipe(uglifycss())
+            .pipe(gulp.dest(targetLocation));
+    };
 
-gulp.task('sass', function () {
+gulp.task('sass', function() {
     sassProcess();
 
 });
 
-gulp.task('copy-assets', function () {
-    
-      gulp.src(['./src/html/imgs/**/*'] )
-              .pipe(gulp.dest(targetLocation+'/images'));
+gulp.task('copy-assets', function() {
 
-       gulp.src(['./src/bower/bootstrap/dist/**/*'] )
-              .pipe(gulp.dest(targetLocation+'/bootstrap'));         
- 
-    
-    
+    gulp.src(['./src/html/imgs/**/*'])
+        .pipe(gulp.dest(targetLocation + '/images'));
+
+    gulp.src(['./src/bootstrap-dist/**/*.min.*'])
+        .pipe(gulp.dest(targetLocation + '/bootstrap'));
+
+
+
 });
 
 
-gulp.task('watch', function () {
-
-    watch(SASS_FILES, function (events, done) {
-
-        sassProcess()
-                .on('finish', function ( ) {
-                    gutil.log("processing change in css");
-                    livereload.reload(pageURL);
-                });
-
+gulp.task('serve', function(cb) {
+    browserSync.init({
+        server: "./public_html",
+        port: 6060,
+        ui: {
+            port: 6061
+        }
     });
 
-    watch(WATCH_JS, function (events, done) {
+    watch(SASS_FILES, function(events, done) {
+
+        sassProcess()
+            .on('finish', function() {
+                gutil.log("processing change in css");
+
+            });
+
+    });
+    // watch src
+    watch(WATCH_JS, function(events, done) {
 
         gulp.start('build');
     });
 
-    watch(MAIN_HTML_FILE, function (events, done) {
+    watch(['./public_html/*.js', './public_html/*.css', './public_html/*.html'], function(events) {
+
+        browserSync.reload();
+        console.log("bundle finished")
+
+    });
+
+    watch(MAIN_HTML_FILE, function(events, done) {
         gutil.log("starting html change");
         gulp.start('copy-html');
     });
 
 });
 
-/*
 
-,
-                    filter: function (filePath, cb) {
-                        if (/bundle.js/.test(filePath)) {
-                            gutil.log("hit js 1")
-                            cb(true)
-                        } else if (/style.min.css/.test(filePath)) {
-                            gutil.log("hit css 1")
-                            cb(true)
-                        }
-                    }
 
-*/
-gulp.task('serve', function (done) {
-    livereload.listen();
-    gulp.src('target')
-            .pipe(server({
-                livereload: {
-                    enable: true
-                },
-                host: '127.0.0.1',
-                port: 8080,
-                defaultFile: 'index.html',
-                directoryListing: false,
-                open: true
-            }));
-});
-gulp.task('release', gulpsync.sync(['clean','build', 'sass']));
-gulp.task('dev', gulpsync.sync(['clean', 'build', 'sass','copy-assets', 'copy-html', 'watch', 'serve']));
+gulp.task('release', gulpsync.sync(['clean', 'build', 'sass']));
+gulp.task('dev', gulpsync.sync(['clean', 'build', 'sass', 'copy-assets', 'copy-html', 'serve']));
